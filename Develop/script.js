12506e880a94514f1ba2872cf6bf493e9238bb16
Virginia <virginia.rabello2107@gmail.com>
2021-04-02 00:19:39 -0500

    Get password length

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,21 @@
 // Assignment code here
 
+// This function request the password's length to the user
+var passwordLength = function(){
+  var length = 0;
+  var endLoop = false;
+  while (endLoop === false){
+    length = prompt("How many characters would you like in your password? Choose a number between 8 and 128.");
+    if (length === "" || length === null || length < 8 || length > 128){
+      alert("You forgot to type a number or typed a invalid number");
+    }
+      else {
+          endLoop = true;
+        }
+  }
+}
+
+passwordLength();
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Password Generator Starter Code
+
+**Password generator**
+
+This is a password generator created to increase security for sensitive data by generating stronger passwords. This application must to attend the following criteria:
+
+- Ask the user to chose the length of the password, it has to be a number from 8 to 128.
+- Ask the user which types of characters the password should have. The options are lowercase, uppercase, numeric, and/or special characters. 
+- Validate the user inputs.
+- Generates a password that matches the user criteria.
+- The password must be displayed in the prompt or written in the page.
